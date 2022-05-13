d149e4332f81d74fa0084732f73fb8f540de36a3
Erik Kroon Celander <ek223ur@student.lnu.se>
2022-05-13 17:01:43 +0200

    test

---
+++ src/server.js
@@ -0,0 +1,87 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Erik Kroon Celander
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import logger from 'morgan'
+import { router } from './routes/router.js'
+import { connectDB } from './config/mongoose.js'
+import helmet from 'helmet'
+import cors from 'cors'
+try {
+  // Connect to MongoDB.
+
+  await connectDB()
+
+  // Creates an Express application.
+  const app = express()
+
+  // Set up a morgan logger using the dev format for log entries.
+  app.use(logger('dev'))
+
+  app.use(express.json())
+  app.use(cors())
+  // Parse requests of the content type application/x-www-form-urlencoded.
+  // Populates the request object with a body object (req.body).
+  app.use(express.urlencoded({ extended: false }))
+
+  app.use(helmet())
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        'script-src': ["'self'"],
+        'style-src': ["'self'"]
+      }
+    })
+  )
+
+  // Register routes.
+  app.use('/', router)
+
+  // Error handler.
+  app.use(function (err, req, res, next) {
+    err.status = err.status || 500
+    if (err.status === 500) {
+      err.message = 'An unexpected condition was encountered.'
+    }
+
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+    }
+    // Development only!
+    // Only providing detailed error in development.
+
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+        cause: err.cause
+          ? {
+              status: err.cause.status,
+              message: err.cause.message,
+              stack: err.cause.stack
+            }
+          : null,
+        stack: err.stack
+      })
+  })
+
+  // Starts the HTTP server listening for connections.
+  app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
